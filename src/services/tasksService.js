185b0a504ade900e44b984c1dd91dba2c6243762
Paulo Nogueira <paulrpn@live.com>
2022-02-17 06:42:36 -0300

    camada de servico

---
+++ src/services/tasksService.js
@@ -0,0 +1,67 @@
+const { ObjectId } = require('mongodb');
+const tasksModel = require('../models/tasksModel');
+
+const ERROR_MSG_1 = {
+  status: 400,
+  message: 'Invalid taskId',
+};
+
+const ERROR_MSG_2 = {
+  status: 404,
+  message: 'Task not found',
+};
+
+const getTasks = async () => {
+  const tasks = await tasksModel.getTasks();
+  return tasks;
+};
+
+const getTaskById = async (id) => {
+  if (!ObjectId.isValid(id)) throw ERROR_MSG_1;
+  const task = await tasksModel.getTaskById(id);
+  if (!task) throw ERROR_MSG_2;
+  return task;
+};
+
+const createTask = async (title, user, status) => {
+  const date = new Date();
+  const { newTaskId } = await tasksModel.createTask(title, user, status, date);
+
+  return {
+    _id: newTaskId,
+    title,
+    user,
+    status,
+    date,
+  };
+};
+
+const updateTask = async (id, bodyData) => {
+  const { title, user, status } = bodyData;
+  const { date } = await getTaskById(id);
+  const updateStatus = await tasksModel.updateTask(id, bodyData);
+
+  if (updateStatus === 0) throw ERROR_MSG_2;
+
+  return {
+    _id: id,
+    title,
+    user,
+    status,
+    date,
+  };
+};
+
+const deleteTask = async (id) => {
+  const deleteStatus = await tasksModel.deleteTask(id);
+  if (deleteStatus === 0) throw ERROR_MSG_2;
+  return null;
+};
+
+module.exports = {
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+};
